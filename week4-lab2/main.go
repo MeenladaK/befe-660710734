cbf1d697f9c7a9b978fb1649a43ccd27477f6ad7
MeenladaK <kaewpinij_m@su.ac.th>
2025-08-06 06:53:13 +0000

    week4-lab2

---
+++ week4-lab2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+// var email string = "meenlada013@gmail.com"
+
+func main() {
+	// var name string = "meenlada"
+	var age int = 20
+
+	email := "meenlada013@gmail.com"
+	gpa := 4.00
+
+	firstname, lastname := "meenlada", "kaewpnij"
+
+	fmt.Printf("Name %s %s,age %d, email %s, gpa %.2f\n", firstname, lastname, age, email, gpa)
+}
