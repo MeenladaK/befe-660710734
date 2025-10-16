f7b25ba3150d675e4c5a42ab923909ed830a2635
MeenladaK <kaewpinij_m@su.ac.th>
2025-10-16 02:59:37 +0000

    เปลี่ยนสี

--- week9-lab2/mybookstore102/tailwind.config.js
+++ week9-lab2/mybookstore102/tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -8,6 +7,9 @@ module.exports = {
       colors: {
         'bookstore-primary': '#2d5a4d',
         'bookstore-secondary': '#5fe9bc',
+        viridian: {
+          600:"#40826D"
+        },
       },
       fontFamily: {
         'sans': ['Prompt', 'sans-serif'],
